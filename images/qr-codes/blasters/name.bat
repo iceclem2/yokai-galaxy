e72bb520393f7cead2a2790d868ba2071e9f67e6
iceclem2 <136616518+iceclem2@users.noreply.github.com>
2024-07-15 11:14:43 +0200

    Added Qr-Codes

--- images/qr-codes/blasters/name.bat
+++ images/qr-codes/blasters/name.bat
@@ -7,7 +7,19 @@ set "numero=1"
 rem Parcours tous les fichiers .png dans le dossier courant
 for %%F in (*.png) do (
     rem Crée le nouveau nom avec le format qr-piece-rouge-XX.png
-    set "nouveauNom=qr-piece-rouge-!numero!.png"
+    set "nouveauNom=file-!numero!.png"
+
+    rem Renomme le fichier
+    ren "%%F" "!nouveauNom!"
+
+    rem Incrémente le compteur
+    set /a "numero+=1"
+)
+
+set "numero=1"
+for %%F in (*.png) do (
+    rem Crée le nouveau nom avec le format qr-piece-rouge-XX.png
+    set "nouveauNom=qr-piece-orange-!numero!.png"
 
     rem Renomme le fichier
     ren "%%F" "!nouveauNom!"
